6420ba83ee56fbc83de0428dfbc7d59b1a864e6f
Guilherme Gentil <gui.cgentil@gmail.com>
2018-06-28 15:21:35 -0300

    2.12b - Adição Middleware "method-override" e "body-parser"

--- config/express.js
+++ config/express.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
+
 var home = require('../app/routes/home');
 
 module.exports = function() {
@@ -14,6 +16,9 @@ module.exports = function() {
 
     //middleware
     app.use(express.static('./public'));
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(require('method-override')());
 
     //routes, configs and assignments
     load('models', {cwd: 'app'})
