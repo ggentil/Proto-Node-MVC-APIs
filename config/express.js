ee3d88385b5c069d04f3f16701301278c607da4a
Guilherme Gentil <gui.cgentil@gmail.com>
2018-06-28 00:37:46 -0300

    2.5 - Configuração de ambiente e middleware

--- config/express.js
+++ config/express.js
@@ -7,7 +7,8 @@ module.exports = function() {
     app.set('port', 3000);
 
     //middleware
-    app.use(express.static('/public'));
+    app.use(express.static('./public'));
 
     return app;
 }
+
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var app = require('./config/express')();
 
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express Server executando na porta ' + app.get('port'));
-});
+http.createServer(app)
+    .listen(app.get('port'), function(){
+        console.log('Express Server executando na porta ' + app.get('port'));
+    });
 
 
 // http.createServer(function(req, res) {
